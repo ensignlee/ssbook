e701d61c88fe557ef048d046f5ea6d030fc81d23
Cameron Davison <cameron.davison+github@gmail.com>
2010-09-19 15:05:13 -0700

    actually get the last odds for a game

--- src/app/models/odd.php
+++ src/app/models/odd.php
@@ -8,9 +8,18 @@ class Odd extends AppModel {
 		$cond = array(
 			'conditions' => array('scoreid' => $scoreid),
 			'order' => array('created DESC'),
-			'group' => array('type')
+			'group' => array('type'),
+			'fields' => array('MAX(id) max_id')
 		);
-
+		$res = $this->find('all', $cond);
+		$ids = array();
+		foreach ($res as $row) {
+			$ids[] = $row[0]['max_id'];
+		}
+	
+		$cond = array(
+			'conditions' => array('id' => $ids)
+		);
 		return $this->find('all', $cond);
 	}
 }
--- src/app/views/bets/ajax_superbar.ctp
+++ src/app/views/bets/ajax_superbar.ctp
@@ -5,9 +5,9 @@ foreach ($scores as $scoreid => $score) {
 ?>
 <div><?= $score['visitor'] ?> @ <?= $score['home'] ?> <?= date('n/j/y g:i A', strtotime($score['game_date'])) ?></div>
 <?php
-	$html = ob_get_contents();
+	$thehtml = ob_get_contents();
 	ob_end_clean();
-	$out[] = array('html' => $html, 'scoreid' => $scoreid);
+	$out[] = array('html' => $thehtml, 'scoreid' => $scoreid);
 }
 
 echo json_encode($out);
