8fe78df091562a43603afd6aeccb24b4a0fe09ca
Cameron Davison <cameron.davison@gmail.com>
2010-11-10 00:35:32 -0600

    removing console debug messages

--- src/app/webroot/js/viewbets.js
+++ src/app/webroot/js/viewbets.js
@@ -5,11 +5,9 @@ $(function() {
 		return str.toLowerCase().replace(/[^a-z]+/g, '_');
 	}
 	function hideAll(buttonEl) {
-		console.debug(buttonEl, $(buttonEl).parents('.buttons'));
 		$(buttonEl).parents('.buttons').find('td').each(function() {
 			if (this != buttonEl) {
 				var text = convertLabel($(this).text());
-				console.debug(this, text);
 				$('.show_'+text).css('display', 'none');
 			}
 		});
